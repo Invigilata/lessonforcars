52728581e7fcd04af2806530ca82ab24793176cd
Invigilata <vernitemnesteam2018@mail.ru>
2024-02-26 19:27:31 +0300

    cars

---
+++ lessonfor.py
@@ -0,0 +1,7 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for current_car in cars:
+    print('Я езжу на автомобиле марки', current_car)
+    cars_count +=10
+    pass
+print('Всего машин', cars_count)
